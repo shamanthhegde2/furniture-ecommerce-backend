8c5dc77854927a20a6a0fcfc75c8e7d20480a197
shamanthhegde2 <shamanthhegde12@gmail.com>
2023-12-25 13:46:28 -0600

    This is the base code for the app

---
+++ src/main/java/com/pks/furnpark/furnitureecommercebackend/FurnitureEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pks.furnpark.furnitureecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureEcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pks/furnpark/furnitureecommercebackend/FurnitureEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pks.furnpark.furnitureecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
